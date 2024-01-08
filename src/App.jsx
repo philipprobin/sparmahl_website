876a90105ee8fe3f81df3425b81fb05605e52b99
philipprobin <philipprobinsowik@gmail.com>
2024-01-08 17:18:44 -0500

    screenshots and newsletter dummy

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
 import { useState } from "react";
 import HeaderDescription from "./components/header-description";
-import iphones from "./assets/iphones.png";
+import Newsletter from "./components/newsletter";
+import AppleAppStoreButton from "./components/apple-app-store-button";
+import GooglePlayStoreButton from "./components/google-play-store-button";
+import home from "./assets/screenshots/home.png";
+import recipe from "./assets/screenshots/recipe.png";
+import search from "./assets/screenshots/search.png";
 
 function App() {
   return (
-    <div className="bg-customBlack  min-h-screen p-4 md:p-20">
+    <div className="bg-customBlack  min-h-screen p-2 md:p-20">
       <div className="container mx-auto grid h-full w-full grid-cols-1 place-items-center lg:grid-cols-2 ">
+        {/*Block 1*/}
         <div className="col-span-1 m-4">
           <HeaderDescription />
         </div>
-        <div >
-          <img src={iphones} alt="iphones" className="w-96 h-auto"/>
+        <div>
+          <img src={home} alt="home" className="w-64 h-auto m-8" />
+        </div>
+        {/*Block 2*/}
+        <div>
+          <img src={recipe} alt="recipe" className="w-64 h-auto m-8" />
+        </div>
+        <div className="col-span-1 m-4">
+          <h1 class="block antialiased tracking-normal font-sans text-4xl sm:text-5xl font-semibold leading-tight text-white ">
+            Jedes Rezept ist deine Chance zu sparen
+          </h1>
+        </div>
+        {/*Block 3*/}
+        <div className="col-span-1 m-4">
+          <h1 class="block antialiased tracking-normal font-sans text-4xl sm:text-5xl font-semibold leading-tight text-white ">
+            Finde das günstigste Produkt aus allen Supermärkten
+          </h1>
+        </div>
+        <div>
+          <img src={search} alt="search" className="w-64 h-auto m-8" />
         </div>
       </div>
+
+      <Newsletter />
+
+      <div className="flex flex-col sm:flex-row items-center justify-center">
+        <AppleAppStoreButton />
+        <GooglePlayStoreButton />
+      </div>
+      
     </div>
   );
 }
