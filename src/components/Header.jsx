aab3853dc659e6b05bb7f9bc45728a0879d40cb5
moritz-staat <moritz.staat@tu-dortmund.de>
2024-03-31 23:56:10 +0200

    Edited Background Color from Header.jsx and did first design things

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,37 +2,37 @@ import React from "react";
 
 const Headerneu = () => {
     return (
-        <header className="bg-gray-800 text-white py-4">
+        <header className="bg-customBlack text-white py-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="flex items-center">
-                    <h1 className="text-xl font-bold">SparMahl</h1>
+                    <h1 className="text-2xl md:text-4xl font-bold" style={{ color: '#8F2F1A' }}>SparMahl</h1>
                 </div>
                 <nav className="flex items-center">
                     <a
                         href="https://www.instagram.com/sparmahlgbr/"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="mx-2"
+                        className="mx-4 inline-block hover:opacity-75"
                     >
-                        <img src="src/assets/instagram-logo.png" alt="Instagram" className="h-6"/>
+                        <img src="src/assets/instagram-logo.png" alt="Instagram" className="h-10 md:h-12 hover:shadow-lg"/>
                     </a>
                     <a
                         href="https://www.linkedin.com/company/sparmahl"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="mx-2"
+                        className="mx-4 inline-block hover:opacity-75"
                     >
-                        <img src="src/assets/linkedin-logo.png" alt="LinkedIn" className="h-6"/>
+                        <img src="src/assets/linkedin-logo.png" alt="LinkedIn" className="h-10 md:h-12 hover:shadow-lg"/>
                     </a>
                     <a
                         href="https://www.facebook.com/profile.php?id=61557298040792"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="mx-2"
+                        className="mx-4 inline-block hover:opacity-75"
                     >
-                        <img src="src/assets/facebook-logo.png" alt="Facebook" className="h-6"/>
+                        <img src="src/assets/facebook-logo.png" alt="Facebook" className="h-10 md:h-12 hover:shadow-lg"/>
                     </a>
-                    <a href="/impressum" className="mx-2">
+                    <a href="/impressum" className="mx-4 inline-block hover:opacity-75">
                         Impressum
                     </a>
                 </nav>
